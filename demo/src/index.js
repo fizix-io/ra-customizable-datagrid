297ea41593ca39e86b83b707bff5b744fc21c5b7
Aksels Ledins <aksels.ledins@gmail.com>
2018-07-17 17:34:01 +0200

    (demo) removed auth & other unnecessary RA features

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,39 +3,20 @@ import 'babel-polyfill';
 import React from 'react';
 import { Admin, Resource } from 'react-admin'; // eslint-disable-line import/no-unresolved
 import { render } from 'react-dom';
-import { Route } from 'react-router';
-import authProvider from './authProvider';
-import comments from './comments';
-import CustomRouteLayout from './customRouteLayout';
-import CustomRouteNoLayout from './customRouteNoLayout';
 import dataProvider from './dataProvider';
 import i18nProvider from './i18nProvider';
 import posts from './posts';
-import users from './users';
 
 render(
   <Admin
-    authProvider={authProvider}
     dataProvider={dataProvider}
     i18nProvider={i18nProvider}
-    title="Example Admin"
+    title="RA - Customizable datagrid"
     locale="en"
-    customRoutes={[
-      <Route
-        exact
-        path="/custom"
-        component={CustomRouteNoLayout}
-        noLayout
-      />,
-      <Route exact path="/custom2" component={CustomRouteLayout} />,
-    ]}
   >
-    {permissions => [
-      <Resource name="posts" {...posts} />,
-      <Resource name="comments" {...comments} />,
-      permissions ? <Resource name="users" {...users} /> : null,
-      <Resource name="tags" />,
-    ]}
+    <Resource name="posts" {...posts} />
+    <Resource name="posts_2" {...posts} />
+    <Resource name="tags" />
   </Admin>,
   document.getElementById('root')
 );
